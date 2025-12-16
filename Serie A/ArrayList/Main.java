76139d00fb4cbe560396e23c4bdc46d1ba253bce
DavideGiovagnoni <davide.giovagnoni@aldini.istruzioneer.it>
2025-12-16 08:08:28 +0000

    array

--- Serie A/ArrayList/Main.java
+++ Serie A/ArrayList/Main.java
@@ -1,27 +1,26 @@
-package cartella;
 public class Main{
         public static void main(String[] args) throws Exception {
         System.out.println("\nArrayList: ");
         
         Lista lista = new Lista();
         
-        lista.addTail(new Nodo("IT: Ciao"));
-        lista.addTail(new Nodo("EN: Hello"));
-        lista.addTail(new Nodo("ES: Hola"));
+        lista.addTail(new Nodo("Ciao"));
+        lista.addTail(new Nodo("Hello"));
+        lista.addTail(new Nodo("Hola"));
         
         lista.addHead(new Nodo("Prova"));
         
         lista.add("Prova 2");
-        lista.add("yolo");
+        lista.add("Prova");
 
-        lista.exists("EN: Hello");
+        lista.exists("Hello");
         lista.exists("Prova 2 ");
 
         lista.remove("Prova ");
         lista.remove("Prova 2 ");
 
         lista.exists("Prova 2 ");
-        lista.exists("IT: Ciao");
+        lista.exists("Ciao");
 
         System.out.println(lista);
     }
